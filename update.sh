8a5c99e048fe6f131769db2321fc24ddf787df4c
YYTU-Aqua <66307999+YYTU-Aqua@users.noreply.github.com>
2020-06-05 06:46:18 +0900

    Add files via upload

--- update.sh
+++ update.sh
@@ -7,18 +7,7 @@ extension=$(zenity --list --width 600 --title "パッケージを選んでくだ
 
 case $extension in
 FireFox) sudo apt install firefox -y
-Wine) sudo dpkg --add-architecture i386 
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
-rm winehq.key
-sudo apt update
-wget https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key
-sudo apt-key add Release.key
-sudo apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./'
-sudo apt update
-
-sudo apt install --install-recommends winehq-stable
+Wine) sudo apt install --install-recommends winehq-stable
 echo $pass
 ;;
 yaa) echo "yaa"
