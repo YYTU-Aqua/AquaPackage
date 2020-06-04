3f5902fe49d92708f315bde54beaa0f661cc631c
YYTU-Aqua <66307999+YYTU-Aqua@users.noreply.github.com>
2020-06-05 06:41:31 +0900

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,24 +1,24 @@
 
 #1) exit
 #2) update
-#といったメニューが表示される。番号で処理を指定する。
-echo "この操作をすると再起動されます"
-zenity --question --width 350  --text "パッケージ情報を更新しますか？" --ok-label "OK" --cancel-label "NO"
-case $? in
-0) 
-pass=$(zenity --forms --width 350 --add-password "パスワード")
-extension=$(zenity --list --title "パッケージを選んでください" --text="パッケージ" --column "パッケージ" --column "ジャンル" FireFox ネット)
-;;
-1) zenity --text-info --filename "/usr/share/common-licenses/GPL-3"
+zenity --text-info --filename "/usr/share/common-licenses/GPL-3"
 pass=$(zenity --forms --add-password "パスワード")
-extension=$(zenity --list --width 600 --title "パッケージを選んでください" --text="パッケージ" --column "パッケージ" --column "ジャンル" FireFox ネット)
-;;
-*) echo "Timeout"
-;;
-esac
+extension=$(zenity --list --width 600 --title "パッケージを選んでください" --text="パッケージ" --column "パッケージ" --column "ジャンル" FireFox ネット Wine exe)
 
 case $extension in
 FireFox) sudo apt install firefox -y
+Wine) sudo dpkg --add-architecture i386 
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+rm winehq.key
+sudo apt update
+wget https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key
+sudo apt-key add Release.key
+sudo apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./'
+sudo apt update
+
+sudo apt install --install-recommends winehq-stable
 echo $pass
 ;;
 yaa) echo "yaa"
